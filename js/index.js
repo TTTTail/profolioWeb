92fc92caac08d45e9b91632defdd5c4e13517df0
TTTTail <TTTTail@users.noreply.github.com>
2018-02-17 03:35:02 +0800

    changes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+
+
+
 window.onload = function(){
 	var iHeight = window.innerHeight;
 	var iWidth = window.innerWidth;
@@ -13,17 +16,15 @@ window.onload = function(){
 	var oBimg = document.getElementById("bImg");
 	var oFlo = document.getElementById("flo");
 	var oList = document.getElementById('list');
-
 	var oLi = document.getElementById('list').getElementsByTagName("li");
-	//var aImg = aLi.getElementsByTagName("img");
 	var oImg = oList.getElementsByTagName("img");
+	var oIntro = document.getElementById('intro');
 
 	
-	//alert(oLin.style.width);
 	oTimg.style.height = iHeight*0.67 + 'px'; 
 	oTimg.style.paddingTop = iHeight*0.25 + 'px'; 
 
-	oBimg.style.height = iHeight*2.32 + 'px'; 
+	oBimg.style.height = iHeight*1.8 + 'px'; 
 
 	oNav.style.width = iWidth*0.25 + 'px';
 	oNav.style.height = oTimg.style.height;
@@ -36,7 +37,7 @@ window.onload = function(){
 	
 	var oLinH = oLin.height;
 	var oFloH = oFlo.height;
-	//alert(oLinH);
+
 	oLin.style.top = (iHeight*0.96-oLinH)+'px';
 	oFlo.style.top = (iHeight*0.73 -oFloH-420)+'px';
 
@@ -44,14 +45,22 @@ window.onload = function(){
 	oList.style.borderRadius = iWidth*0.05 +'px';
 	oBimg.style.paddingTop = iHeight*0.16 + 'px';
 
+	oIntro.style.width = iWidth + 'px';
+	oIntro.style.height = iWidth*0.2 + 'px';
+	oIntro.style.textAlign = 'center';
+	oIntro.style.paddingTop = iHeight*0.26 + 'px';
+	oIntro.style.paddingBottom = iHeight*0.03 + 'px';
+
 
 	for (var i = oImg.length - 1; i >= 0; i--) {
 		oImg[i].style.width = 320 + 'px';
 		oImg[i].style.height = 'auto';
 	};
 
+	document.getElementById('cir').style.borderWidth = iWidth*0.005 +'px';
 
 }
+	var oAd=['sleepyhead.html','pioneer.html','phone.html','kindle.html','alley.html','others.html']
 	var turn= function(num){
 		window.open(oAd[num])	}
 
